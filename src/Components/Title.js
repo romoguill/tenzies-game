14950f6abf9bada128a6d0ba172e7a8cc0348168
romoguill <romoguill@gmail.com>
2022-07-26 17:40:44 -0300

    Cleaned the folders and set up the Title component

---
+++ src/Components/Title.js
@@ -0,0 +1,13 @@
+function Title() {
+  return (
+    <section className="section--game_description">
+      <h1>Tenzies</h1>
+      <p>
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+    </section>
+  );
+}
+
+export default Title;
