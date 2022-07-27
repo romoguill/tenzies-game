82533a7f6d6c88fe41404b16559e24707dbd7404
romoguill <romoguill@gmail.com>
2022-07-27 06:15:28 -0300

    Created Die component and styled it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './styles.css';
 import Title from './Components/Title';
+import Die from './Components/Die';
 
 function App() {
   return (
     <main className="App">
       <div className="container">
         <Title />
+        <section className="section--dice">
+          <Die value="2" />
+          <Die value="5" />
+          <Die value="1" />
+          <Die value="2" />
+          <Die value="5" />
+          <Die value="6" />
+          <Die value="4" />
+          <Die value="1" />
+          <Die value="4" />
+          <Die value="3" />
+        </section>
       </div>
     </main>
   );
---
+++ src/Components/Die.js
@@ -0,0 +1,9 @@
+function Die(props) {
+  return (
+    <div className="Die">
+      <h2>{props.value}</h2>
+    </div>
+  );
+}
+
+export default Die;
