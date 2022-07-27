b0d16e58a1a732d67038eec0cbf1568dc1ffff1b
romoguill <romoguill@gmail.com>
2022-07-27 06:31:02 -0300

    Use a function to generate random dice and update the state of Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import './styles.css';
 import Title from './Components/Title';
 import Die from './Components/Die';
+import { useState } from 'react';
 
 function App() {
+  const [dice, setDice] = useState(allNewDice());
+
+  const diceElements = dice.map((die, index) => (
+    <Die key={index} value={die} />
+  ));
+
+  function allNewDice() {
+    const diceArray = [];
+    for (let i = 0; i < 10; i++) {
+      diceArray.push(Math.floor(Math.random() * 6) + 1);
+    }
+    return diceArray;
+  }
+
   return (
     <main className="App">
       <div className="container">
         <Title />
-        <section className="section--dice">
-          <Die value="2" />
-          <Die value="5" />
-          <Die value="1" />
-          <Die value="2" />
-          <Die value="5" />
-          <Die value="6" />
-          <Die value="4" />
-          <Die value="1" />
-          <Die value="4" />
-          <Die value="3" />
-        </section>
+        <section className="section--dice">{diceElements}</section>
       </div>
     </main>
   );
